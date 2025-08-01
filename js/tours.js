13f08499d3659e9f28469da44f83a98368af70f1
tony223hk <tony223hk2006@yahoo.com.hk>
2025-08-01 17:20:15 +0800

    final version

---
+++ js/tours.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.querySelector(".navbar .mobile-menu-toggle");
+    const mobileMenu = document.querySelector(".navbar .mobile-menu-items");
+
+toggleButton.addEventListener("click", () => {
+    mobileMenu.classList.toggle("active");
+    });
+});
+
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
